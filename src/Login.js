dc0e54c9e4bef7967354cfad790772985527b53b
Sundar_Charan <81733442+sundarcharan@users.noreply.github.com>
2023-01-06 15:41:24 +0530

    demo with bootstrap

--- src/Login.js
+++ src/Login.js
@@ -28,6 +28,7 @@ export default function Login() {
           Login
         </Button>
       </Form>
+      <hr />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Button from "react-bootstrap/Button";
-
 import Login from "./Login";
 
 const rootElement = document.getElementById("root");
